144b8d5a33837dee85dd40a19d3d10013fb2065b
triplek <triplek@debian>
2021-07-02 08:51:49 +0530

    casual edits

--- fish/config.fish
+++ fish/config.fish
@@ -13,30 +13,13 @@ end
 function clear
 	command /usr/bin/colorscript -e elfman
 end
+function neovide
+	command neovide > /dev/null 2>&1 $argv
+end
 
 #set -x MANPAGER "sh -c 'col -bx | bat -l man -p'" 
- set -x MANPAGER "nvim -c 'set ft=man' -"
-
-
-### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
-set fish_color_normal '#8be9fd'
-set fish_color_command '#8be9fd'
-set fish_color_quote '#f1fa8c'
-set fish_color_redirections '#ffb86c'
-set fish_color_end '#ff79c6'
-set fish_color_error '#ff5555'
-set fish_color_param '#50fa7b'
-set fish_color_fish_color_comment '#ff79c6'
-set fish_color_match '#6272a4'
-set fish_color_search_match '#f1fa8c'
-set fish_color_operator '#50fa7b'
-set fish_color_escape '#bd93f9'
-set fish_color_cwd '#ffb86c'
-set fish_pager_color_prefix '#8be9fd'
-set fish_pager_color_completion '#50fa7b'
-set fish_pager_color_description '#bd93f9'
-set fish_pager_color_progress '#ffb86c'
-set fish_color_autosuggestion '#6272a4'
+set -x MANPAGER "nvim -c 'set ft=man' -"
+set PATH $PATH /home/triplek/.local/bin /usr/bin /usr/local/bin /usr/sbin /usr/local/sbin /bin /sbin $HOME/.fzf/bin $HOME/.cargo/bin $HOME/.config/scripts $HOME/spicetify-cli
 
 set fish_greeting
 fish_vi_key_bindings
